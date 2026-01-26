45eaf6c51efaeda5e6be312d2cef931febef8127
fchacon <fchacon@bnamericas.com>
2026-01-26 11:41:59 -0300

    modify contactRoutes

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,6 +1,2 @@
 // Detecta si estamos en desarrollo o producción
-const IS_PROD = import.meta.env.PROD; 
-
-export const API_BASE_URL = IS_PROD 
-  ? 'https://tu-backend-en-render.com'  // URL de Producción
-  : 'http://localhost:5000';           // URL Local
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
